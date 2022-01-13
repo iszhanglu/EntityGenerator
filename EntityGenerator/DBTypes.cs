396f0ff906eb5ed1ad399e3034a8f0863d9e785d
iszhanglu <iszhanglu@outlook.com>
2022-01-13 14:18:44 +0800

    添加项目文件。

---
+++ EntityGenerator/DBTypes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityGenerator
+{
+    internal class DBType
+    {
+        public string name { get; set; }
+        public string value { get; set; }
+    }
+
+    internal class DBTypes
+    {
+        public List<DBType> dbtypes { get; set; }
+        public DBTypes()
+        {
+            dbtypes = new List<DBType> {
+                new DBType{name="MySql",value ="0" },
+                new DBType{name="SqlServer",value ="1" },
+                new DBType{name="Sqlite",value ="2" },
+                new DBType{name="Oracle",value ="3" },
+                new DBType{name="PostgreSQL",value ="4" },
+                new DBType{name="Dm",value ="5" },
+                new DBType{name="Kdbndp",value ="6" },
+                new DBType{name="Oscar",value ="7" }
+            };
+        }
+    }
+
+}
---
+++ EntityGenerator/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EntityGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;// 窗体居中
+
+            InitializeComponent();
+
+            DBTypes dBTypes = new DBTypes();
+            this.DataContext = dBTypes;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string dbstring = $"server={txtHost.Text};uid={txtAccount.Text};pwd={txtPassword.Text};database={txtDB.Text}";
+            DbType dbtype = (DbType)cbType.SelectedIndex;
+            string snamespace = txtNamespace.Text;
+
+            txtTip.Text = "生成中...";
+            txtTip.Foreground = Brushes.Brown;
+
+            btnCreate.IsEnabled = false;
+            new Thread(() =>
+            {
+                DBRun(dbstring, dbtype, snamespace);
+            }).Start();
+
+        }
+
+        private void DBRun(string dbstring, DbType dbtype, string snamespace)
+        {
+            try
+            {
+
+                SqlSugarClient db = new SqlSugarClient(new ConnectionConfig()
+                {
+                    ConnectionString = dbstring,//连接符字串
+                    DbType = dbtype,
+                    IsAutoCloseConnection = true //不设成true要手动close
+                }); ;
+
+                string path = $"{Environment.CurrentDirectory}\\Entity";
+                db.DbFirst.IsCreateAttribute().CreateClassFile(path, snamespace);
+
+                this.Dispatcher.Invoke(new Action(() => {
+                    txtTip.Text = "成功生成到Entity目录下！";
+                    txtTip.Foreground = Brushes.Green;
+                }));
+                
+            }
+            catch (Exception ex)
+            {
+                this.Dispatcher.Invoke(new Action(() => {
+                    txtTip.Text = "生成失败！" + ex.Message;
+                    txtTip.Foreground = Brushes.Red;
+                }));
+                
+            }
+            this.Dispatcher.Invoke(() => {
+                btnCreate.IsEnabled = true;
+            });
+            
+
+        }
+    }
+}
